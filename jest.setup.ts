bc23ce46359e70f09f54898b8255c00593cab0b3
luiznds62 <luiznds62@gmail.com>
2021-03-15 21:48:46 -0300

    Log level in tests changed to fatal

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,5 @@
 module.exports = async () => {
   process.env.SECURITY_ISS = 'safecrossing-api';
   process.env.SECURITY_API_SECRET = 'f1fdeaf03bbc0f0134bfb24db9cd9989';
-  process.env.SERVER_LOG_LEVEL = 'error';
+  process.env.SERVER_LOG_LEVEL = 'fatal';
 };
