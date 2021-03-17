16c1ebbcb3a43421b5f11bd5b09a0590f35cde8a
Luiz Henrique Naspolini <luiznds62@gmail.com>
2021-03-17 18:27:10 -0300

    Delete node_modules directory

--- node_modules/typescript-ioc/dist/container/container-types.d.ts
+++
@@ -1,3 +0,0 @@
-import { BuildContext } from '../model';
-export declare type InstanceFactory = (source: Function, context: BuildContext) => any;
-export declare type ValueFactory = (source: string) => any;
